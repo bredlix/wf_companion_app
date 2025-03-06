6ed1ffa1fdb64c5ee92a99f9d36e13ae346f43c7
Hsiang Chou <hsiang307@gmail.com>
2025-03-06 15:43:03 +0000

    Update compileSdk to 35 & fix Material dependency issue

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.android.wf_companion_app"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.android.wf_companion_app"
